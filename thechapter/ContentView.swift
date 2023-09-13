2e8067e4138d761532585c932f21336b84042476
Arnon Keereena <arnon.keereena@accenture.com>
2023-09-13 15:19:11 +0700

    Initial Commit

---
+++ thechapter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  thechapter
+//
+//  Created by arnon.keereena on 13/9/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ thechapter/thechapterApp.swift
@@ -0,0 +1,17 @@
+//
+//  thechapterApp.swift
+//  thechapter
+//
+//  Created by arnon.keereena on 13/9/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct thechapterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
